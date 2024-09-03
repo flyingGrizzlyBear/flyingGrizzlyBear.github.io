9dbdfe5d1c99387c3577c633a498638b0e93140b
flyingGrizzlyBear <99915669+flyingGrizzlyBear@users.noreply.github.com>
2024-09-03 18:55:51 +0200

    index.js: added func no right click on img

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,17 @@ const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-})
+});
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    })
+    });
 })
+
+// Disable right-click on all images
+document.addEventListener('contextmenu', function(e) {
+    if (e.target.tagName === 'IMG') {
+        e.preventDefault();
+    }
+});
